260324647193795928fa259ea7d7cf5e36a8053d
amrahmed <amrahmedsaid969@gmail.com>
2022-10-12 20:09:06 +0200

    Authentication

--- API/Controllers/USerController.cs
+++ API/Controllers/USerController.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class USersController : ControllerBase
+   
+    public class USersController : BaseApiController
     {
         private readonly DataContext _context;
         public USersController(DataContext context)
@@ -20,16 +20,19 @@ namespace API.Controllers
         }
 
         [HttpGet]
+        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await _context.Users.ToListAsync();
         }
 
+        [Authorize]
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
             return await _context.Users.FindAsync(id);
         }
+        
 
     }
 }
---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection ApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+             // add Token service
+            services.AddScoped<ITokenService,TokenService>();
+            services.AddDbContext<DataContext>(optios=>
+            {
+                optios.UseSqlite(config.GetConnectionString("DefaultConnection"));
+
+            });
+            return services;
+        }
+        
+    }
+}
